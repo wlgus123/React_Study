e7ea9e56732e3441ddadc985ef7ea66fb32a9d80
wlgus123 <mail30019794@gmail.com>
2024-01-15 15:56:35 +0900

    [:sparkles:] props

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
 import './App.css';
 
 // 컴포넌트
-function Header() {
+function Header(props) {
   return <header>
-    <h1>
-      <a href='/'>React</a>
-    </h1>
+    <h1><a href='/'>{props.title}</a></h1>
   </header>
 }
 
-function Nav() {
+function Nav(props) {
+  const lis = []
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i]
+    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)
+    // key={t.id}를 줌으로써 고유 id를 가지게 됨
+  }
   return <nav>
     <ol>
-      <li><a href='/read/1'>html</a></li>
-      <li><a href='/read/2'>css</a></li>
-      <li><a href='/read/3'>js</a></li>
+      {lis}
     </ol>
   </nav>
 }
 
-function Article() {
+function Article(props) {
   return <article>
-    <h2>Welcome</h2>
-    Hello, WEB
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html is...' },
+    { id: 2, title: 'css', body: 'css is...' },
+    { id: 3, title: 'js', body: 'js is...' },
+  ]
   return (
     <div>
-      <Header />
-      <Header />  {/* 한 코드를 여러 개 복사하거나 여러 개의 코드를 수정할 때에도 용이함 */}
-      <Nav />
-      <Article />
+      <Header title="REACT" />
+      <Nav topics={topics} />  {/* 있는 그대로 전달하기 위해 중괄호({})를 사용 */}
+      <Article title="Welcome" body="Hello, WEB" />
+      <Article title="Hi" body="Hello, React" />  {/* 같은 코드라도 다르게 출력할 수 있음 */}
     </div>
   );
 }
