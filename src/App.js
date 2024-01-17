654297e297ca0b7a4b8a367154628a488e3f7f41
wlgus123 <mail30019794@gmail.com>
2024-01-17 17:31:05 +0900

    [:sparkles:] 이벤트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
 // 컴포넌트
+function Article(props) {
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
 function Header(props) {
   return <header>
-    <h1><a href='/'>{props.title}</a></h1>
+    <h1><a href='/' onClick={(event) => {
+      event.preventDefault();  // 기본 동작 방지 (클릭해도 reload가 일어나지 않음)
+      props.onChangeMode();  // onChangeMode 함수를 호출함
+    }}>{props.title}</a></h1>
   </header>
 }
 
@@ -11,21 +21,19 @@ function Nav(props) {
   const lis = []
   for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i]
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)
-    // key={t.id}를 줌으로써 고유 id를 가지게 됨
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);  // target == a태그
+      }}>{t.title}</a>
+      {/* key={t.id}를 줌으로써 고유 id를 가지게 됨 */}
+    </li>)
   }
   return <nav>
     <ol>
       {lis}
     </ol>
   </nav>
-}
-
-function Article(props) {
-  return <article>
-    <h2>{props.title}</h2>
-    {props.body}
-  </article>
 }
 
 function App() {
@@ -36,10 +44,13 @@ function App() {
   ]
   return (
     <div>
-      <Header title="REACT" />
-      <Nav topics={topics} />  {/* 있는 그대로 전달하기 위해 중괄호({})를 사용 */}
+      <Header title="WEB" onChangeMode={() => {
+        alert('Header');
+      }} />
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id);
+      }} />  {/* 있는 그대로 전달하기 위해 중괄호({})를 사용 */}
       <Article title="Welcome" body="Hello, WEB" />
-      <Article title="Hi" body="Hello, React" />  {/* 같은 코드라도 다르게 출력할 수 있음 */}
     </div>
   );
 }
